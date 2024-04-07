b912a97045d92f771c99be9d76b40965f0b87cbc
abhishek mutkekar <abhishekmutkekar@kccemsr.edu.in>
2024-04-07 20:06:02 +0530

    Package Installation

---
+++ Smart_Resume_Analyzer.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import base64, random 
+import time,datetime
+from pyresparser import ResumeParser 
+from pdfminer3.layout import LAParams, LLTestBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io,random
+from streamlit_tags import st_tags
+from PIL import Image
+import pymysql
+from Courses import ds_course,web_courses,android_course,ios_course,uiux_course
+import pafy 
+import plotly.express as px 
+
+
+connection = pymysql.connect(host='localhost',user='root',password='',db='sra')
+cursor = connection.cursor()
